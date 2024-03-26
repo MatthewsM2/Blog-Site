b228475d2fadfaba696209a54b42527ab0d6e3b1
Matthew M <matthew17773@gmail.com>
2024-03-26 20:11:51 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function NavBar()
+{
+  return (
+    <div className='nav-container'>
+      <h3>Lab App</h3>
+  <ul className='nav-body'>
+    <li className='nav-icons'>Home</li>
+    <li className='nav-icons'>Blog</li>
+    <li className='nav-icons'>Sign in</li>
+  </ul>
+  </div>
+  );
+}
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NavBar/>
     </div>
   );
 }
 
-export default App;
+// export default App;
