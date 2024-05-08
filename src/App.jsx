d958ba267812d2a0d09e1a31ecb59ac178b7621c
Matthew M <matthew17773@gmail.com>
2024-05-08 23:43:57 +0530

    input box

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   const hour = date.getHours();
   return (
     <>
+    <Login/>
       <h1> {message} </h1>
 	  <h5> React App </h5>
     <h5> { date.getHours() } </h5>
