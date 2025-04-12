c5ccd394b5e653ae9b9ed990ce5c47608cb2f084
MatthewsM2 <matthew17773@gmail.com>
2025-04-12 12:31:32 +0530

    Build Files

--- content/_index.md
+++
@@ -1,5 +0,0 @@
----
-title: "Home"
----
-
-# Hi there 👋
--- themes/custom/static/scripts/script.js
+++
@@ -1,7 +0,0 @@
-function uptime() {
-    console.log("hello hai");
-    if(localStorage.uptime) return;
-    const time = Date.now;
-    console.log(time);
-    localStorage.setItem('uptime',time);
-}
